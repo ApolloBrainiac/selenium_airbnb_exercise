ad6f3b72ec878e0230d421423fb6cfec4ffd8c59
Andrew M. Perez <AxMPerez@gmail.com>
2018-03-14 18:44:29 -0400

    Set up classes and function for test

---
+++ airbnb_test.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import time
+
+
+class airBnb():
+
+	def test(self):
+
+
+
+ab = airBnb()
+ab.test()
