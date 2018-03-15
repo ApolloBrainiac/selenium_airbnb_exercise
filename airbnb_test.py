b0b60bdaf558cb0457b34a433ba69dace22b8396
Andrew M. Perez <AxMPerez@gmail.com>
2018-03-15 11:35:33 -0400

    Refine xpaths and add date randomization

--- airbnb_test.py
+++ airbnb_test.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 import time
+import random
 
 
 class airBnb():
@@ -14,12 +15,13 @@ class airBnb():
         driver.implicitly_wait(10)
 
         def pause():
-        	time.sleep(4)
+            time.sleep(4)
 
         # Landing page steps
         searchBar = driver.find_element(By.ID,
-        'GeocompleteController-via-SearchBarV2-SearchBarV2')
-        searchButton = driver.find_element(By.XPATH, '//button[@type="submit"]')
+                                        'GeocompleteController-via-SearchBarV2-SearchBarV2')
+        searchButton = driver.find_element(
+            By.XPATH, '//button[@type="submit"]')
 
         searchBar.send_keys("London")
         pause()
@@ -28,14 +30,20 @@ class airBnb():
 
         # Date Selection Steps
         dateButton = driver.find_element(By.XPATH,
-        	'//span[contains(text(), "Dates"]')
+                                         '//button[@role="menuitem"]//span[contains(text(), "Dates")]')
         applyButton = driver.find_element(By.XPATH,
-        	'//span[contains(text(), "Apply"]')
-        dateButton.cick()
+                                          '//span[contains(text(), "Apply")]')
+
+        dateButton.click()
         pause()
 
-        date1 = driver.find_element(By.XPATH, '//td[contains(text(), "15"]')
-        date2 = driver.find_element(By.XPATH, '//td[contains(text(), "23"]')
+        ran1 = random.randint(15, 22)
+        ran2 = random.randint(23, 30)
+
+        date1 = driver.find_element(
+            By.XPATH, '//td[contains(text(), "%s"]' % str(ran1))
+        date2 = driver.find_element(
+            By.XPATH, '//td[contains(text(), "%s"]' % str(ran2))
 
         date1.click()
         pause()
